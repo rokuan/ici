b5270c874a1deb9ac2cc0820877150cf017cefd9
LEBEAU Christophe <c.lebeau23@gmail.com>
2016-10-30 23:26:47 +0100

    added the skeleton of the main page

--- app/src/main/java/com/ideal/apps/ici/MainActivity.java
+++ app/src/main/java/com/ideal/apps/ici/MainActivity.java
@@ -1,13 +1,37 @@
 package com.ideal.apps.ici;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.login_text)
+    protected EditText login;
+    @BindView(R.id.password_text)
+    protected EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.connect_button)
+    public void connect(){
+        if(login.getText().toString().isEmpty()){
+            // TODO:
+            return;
+        }
+        if(password.getText().toString().isEmpty()){
+            return;
+        }
+        Intent i = new Intent(this, MapsActivity.class);
+        this.startActivity(i);
     }
 }
